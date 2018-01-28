edfd64f37bca9ef510ca8d51b841599190680cb9
Unknown <robert@zzyzxl.com>
2018-01-28 08:20:36 +0000

    333333333333333

---
+++ First Folder/uFrmMain - Copy (4).pas
@@ -0,0 +1,26 @@
+unit uFrmMain;
+
+interface
+1111111111111111111
+22222222222222222222
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm5 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ First Folder/uZNodeCollection - Copy (3).pas
@@ -0,0 +1,109 @@
+unit uZNodeCollection;
+
+interface
+
+uses
+  System.Classes;
+
+
+type
+//  IZMemNode = interface;
+  TZMemNode<TClassOfType> = class;
+
+  IZMemNode<TClassOfType> = interface
+    function GetValue: TClassOfType;
+    property Value: TClassOfType read GetValue;
+  end;
+
+
+  TZMemNode<TClassOfType> = class(TInterfacedObject, IZMemNode<TClassOfType>)
+
+  private
+    FValue: TClassOfType;
+    function GetValue: TClassOfType;
+    procedure SetValue(const Value: TClassOfType);
+  public
+    property Value: TClassOfType read GetValue write SetValue;
+  end;
+
+  TZBaseClassList = class(TList)
+  private
+
+  dddd
+  dddd
+  ddddd
+  
+  public
+    constructor Create;
+  end;
+
+  sdasdas
+  sdasdas
+
+  TZMemNodeList<TClassOfType> = class(TZBaseClassList)
+  private
+    FValues: array of TZMemNode<TClassOfType>;
+    function GetValues(Index: Integer): TZMemNode<TClassOfType>;
+    procedure SetValues(Index: Integer; const Value: TZMemNode<TClassOfType>);
+
+  public
+    constructor Create;
+    property Values[Index: Integer]: TZMemNode<TClassOfType> read GetValues write
+        SetValues;
+  end;
+
+
+
+
+
+var
+  InitialMemCacheSize : Integer = 64;
+  GrowMemCacheSize : Integer = 64;
+
+implementation
+
+function TZMemNode<TClassOfType>.GetValue: TClassOfType;
+begin
+  Result := FValue;
+end;
+
+procedure TZMemNode<TClassOfType>.SetValue(const Value: TClassOfType);
+begin
+  FValue := Value;
+end;
+
+constructor TZMemNodeList<TClassOfType>.Create;
+begin
+  inherited Create;
+  SetLength(FValues, InitialMemCacheSize);
+end;
+
+function TZMemNodeList<TClassOfType>.GetValues(Index: Integer):
+    TZMemNode<TClassOfType>;
+var
+  obj: IZMemNode<Integer>;
+  xx : IInterface;
+begin
+  obj := TZMemNode<Integer>.Create;
+  Add(obj);
+  Add(Self);
+  Add(xx);
+  xx := IInterface(Self[0]);
+  obj := TZMemNode<Integer>(Self[0]);
+  Result := FValues[Index];
+end;
+
+procedure TZMemNodeList<TClassOfType>.SetValues(Index: Integer; const Value:
+    TZMemNode<TClassOfType>);
+begin
+  FValues[Index] := Value;
+end;
+
+constructor TZBaseClassList.Create;
+begin
+  inherited Create;
+end;
+
+
+
+end.
